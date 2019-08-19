492feded4086a8ff0c64a2414e768e643fd33124
Evgeniy Makarov <makarov.e@atu.su>
2019-08-20 00:23:42 +0300

    first code

---
+++ new project/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое число");
+            string x = Console.ReadLine();
+            int a = Convert.ToInt32(x);
+            if (a < 5)
+            {
+                a = a * a;
+            }
+            else
+            {
+                a = a * 10;
+            }    
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
